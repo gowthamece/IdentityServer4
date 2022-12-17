7f209fbbd2d7fcab769545a34b5640b6dad4cd67
Gowtham <gowthamk91@outlook.com>
2022-12-17 10:00:00 +0530

    Updated EF Core Project

--- IdentityServerEFCore/Program.cs
+++ IdentityServerEFCore/Program.cs
@@ -1,4 +1,7 @@
 using IdentityServer4.Configuration;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServerEFCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using System.Configuration;
@@ -8,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 string connectionString = builder.Configuration.GetConnectionString("localdb");
 
 var migrationsAssembly = typeof(Program).Assembly.GetName().Name;
+
 
 builder.Services.AddIdentityServer()
 .AddConfigurationStore(options =>
@@ -24,8 +28,55 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-
-
+if(app.Environment.IsDevelopment())
+{
+    InitializeDatabase(app);
+}
 app.UseRouting();
 app.UseIdentityServer();
 app.Run();
+
+static void InitializeDatabase(IApplicationBuilder app)
+{
+    using (var serviceScope =
+            app.ApplicationServices
+                .GetService<IServiceScopeFactory>().CreateScope())
+    {
+        serviceScope
+            .ServiceProvider
+                .GetRequiredService<PersistedGrantDbContext>()
+                .Database.Migrate();
+
+        var context =
+            serviceScope.ServiceProvider
+                .GetRequiredService<ConfigurationDbContext>();
+
+        context.Database.Migrate();
+        if (!context.Clients.Any())
+        {
+            foreach (var client in Config.Clients)
+            {
+                context.Clients.Add(client.ToEntity());
+            }
+            context.SaveChanges();
+        }
+
+        if (!context.IdentityResources.Any())
+        {
+            foreach (var resource in Config.Ids)
+            {
+                context.IdentityResources.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+
+        if (!context.ApiResources.Any())
+        {
+            foreach (var resource in Config.Apis)
+            {
+                context.ApiResources.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+    }
+}
